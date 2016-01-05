a22b77cbf0df776aeea969caa2a60cab84d98739
vvakame <vvakame+dev@gmail.com>
2016-01-06 01:46:24 +0900

    add main gearpower and brand strength view

--- public/component/searchResultRow.ts
+++ public/component/searchResultRow.ts
@@ -3,15 +3,16 @@
 import {Component, Input} from 'angular2/core';
 import {CORE_DIRECTIVES} from "angular2/common";
 
+import {Gear} from "../lib/model/model";
 import {Result} from "../lib/model/finder";
 
 @Component({
     selector: 'search-result-row',
     template: `
         <div>
-            アタマ：{{result.headgear.name.ja}}<br>
-            フク　：{{result.clothing.name.ja}}<br>
-            クツ　：{{result.shoes.name.ja}}<br>
+            アタマ：{{result.headgear.name.ja}} {{gearpowerMain(result.headgear)}} ブランド：{{brandStrengthString(result.headgear)}}<br>
+            フク　：{{result.clothing.name.ja}} {{gearpowerMain(result.clothing)}} ブランド：{{brandStrengthString(result.clothing)}}<br>
+            クツ　：{{result.shoes.name.ja}} {{gearpowerMain(result.shoes)}} ブランド：{{brandStrengthString(result.shoes)}}<br>
             確率　：{{result.odds}}
         </div>
     `,
@@ -19,4 +20,15 @@ import {Result} from "../lib/model/finder";
 })
 export default class SearchResultRow {
     @Input() result: Result;
+
+    gearpowerMain(gear: Gear) {
+        return `⤴${gear.main.name.ja}`;
+    }
+
+    brandStrengthString(gear: Gear) {
+        if (!gear.brand.strength) {
+            return "ブランド適正無し";
+        }
+        return `⤴${gear.brand.strength.name.ja}`;
+    }
 }
